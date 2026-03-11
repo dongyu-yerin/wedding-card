079b1566ecfbd0ba5238e885acde0997e85ea8de
ddongyu.kim <ddongyu.kim@samsung.com>
2026-03-11 23:08:34 +0900

    update

--- src/component/shareButton/index.tsx
+++ src/component/shareButton/index.tsx
@@ -25,7 +25,9 @@ export const ShareButton = () => {
           }
 
           kakao.Share.sendDefault({
-            objectType: "feed",
+            objectType: "location",
+            address: SHARE_ADDRESS,
+            addressTitle: SHARE_ADDRESS_TITLE,
             content: {
               title: `${GROOM_FULLNAME} ❤️ ${BRIDE_FULLNAME}의 결혼식에 초대합니다.`,
               description:
